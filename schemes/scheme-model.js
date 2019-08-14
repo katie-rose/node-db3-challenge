8b9bccce00221198c6055dc551f9a9826d22050b
Katie Rose <katiefelten@gmail.com>
2019-08-14 15:58:24 -0400

    :rocket: added remaining helpers

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,5 +1,52 @@
 const db = require("../data/dbConfig");
 
+async function find() {
+  return await db("schemes");
+}
+
+async function findById(id) {
+  return (
+    (await db("schemes")
+      .where({ id })
+      .first()) || null
+  );
+}
+
+async function findSteps(id) {
+    return await db("schemes")
+    .join("steps", "steps.scheme_id", "schemes.id")
+    .select(
+        "steps.id",
+        "schemes.scheme_name",
+        "steps.step_number",
+        "steps.instructions"
+    )
+    .where("schemes.id", id)
+}
+
+async function add(scheme) {
+    const id = await db("schemes").insert(scheme, "id");
+    return findById(...id);
+}
+
+async function update(changes, id) {
+    await db("schemes")
+    .where({ id })
+    .update(changes);
+    return findById(id);
+}
+
+async function remove(id) {
+    const delScheme = await findById(id);
+    const del_ = await db("schemes")
+    .where({ id })
+    .del();
+    return del_ ? delScheme : null;
+}
+
+// Stretch
+// async function addStep() {}
+
 module.exports = {
   find,
   findById,
@@ -9,19 +56,3 @@ module.exports = {
   update,
   remove
 };
-
-async function find() {
-  return await db("schemes");
-}
-
-async function findById(id) {}
-
-async function findSteps() {}
-
-async function add() {}
-
-async function addStep() {}
-
-async function update() {}
-
-async function remove() {}
